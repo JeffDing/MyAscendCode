3f8d0b5a252a2ac93f9d17b2536ee972386f0614
YiChao Ding <jeffding890430@163.com>
2024-12-20 15:09:32 +0800

    Create bit.py

---
+++ OpenMind/bit.py
@@ -0,0 +1,59 @@
+from openmind import AutoTokenizer, AutoModelForCausalLM, is_torch_npu_available
+from openmind_hub import snapshot_download
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        help="Path to model",
+        default="jeffding/csg-wukong-1B-sft-dpo-bf16-openmind",
+    )
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    model_path = args.model_name_or_path
+
+    if is_torch_npu_available():
+        device = "npu"
+    else:
+        device = "cpu"
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_path,
+        torch_dtype="auto",
+        device_map="auto"
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+    prompt = "Give me a short introduction to large language model."
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
+    ]
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+    generated_ids = model.generate(
+        model_inputs.input_ids,
+        max_new_tokens=512
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
