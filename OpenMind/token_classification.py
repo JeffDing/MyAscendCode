20afec286b60efe5f96c091fbbdd501fb78d79b3
YiChao Ding <jeffding890430@163.com>
2024-12-24 08:59:25 +0800

    Create token_classification.py

---
+++ OpenMind/token_classification.py
@@ -0,0 +1,41 @@
+from openmind import pipeline, is_torch_npu_available
+from openmind import AutoTokenizer, AutoModelForCausalLM
+from openmind_hub import snapshot_download
+import torch.nn.functional as F
+from torch import Tensor
+import openmind
+import torch
+import argparse
+import time
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        help="Path to model",
+        default="pretrainmodel/bert-base-parsbert-ner-uncased",
+    )
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    model_path = args.model_name_or_path
+
+    if is_torch_npu_available():
+        device = "npu:0"
+    else:
+        device = "cpu"
+    
+    start_time = time.time()
+    
+    nlp = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple", device=device)
+    output = nlp("Apple est créée le 1er avril 1976 dans le garage de la maison d'enfance de Steve Jobs à Los Altos en Californie par Steve Jobs, Steve Wozniak et Ronald Wayne14, puis constituée sous forme de société le 3 janvier 1977 à l'origine sous le nom d'Apple Computer, mais pour ses 30 ans et pour refléter la diversification de ses produits, le mot « computer » est retiré le 9 janvier 2015.")
+    print(f'>>>output={output}')
+    
+    end_time = time.time()
+    print(f"硬件环境：{device},推理执行时间：{end_time - start_time}秒")
+    
+if __name__ == "__main__":
+    main()
