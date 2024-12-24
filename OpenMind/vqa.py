061aee0d18974bc0e5bd30331d60ba5096aa9bdf
YiChao Ding <jeffding890430@163.com>
2024-12-24 16:03:55 +0800

    Create vqa.py

---
+++ OpenMind/vqa.py
@@ -0,0 +1,53 @@
+from openmind import pipeline , is_torch_npu_available
+from openmind_hub import snapshot_download
+import requests
+from PIL import Image
+
+import openmind
+import torch
+import argparse
+import time
+
+# Mean Pooling - Take attention mask into account for correct averaging
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        help="Path to model",
+        default="pretrainmodel/vilt-b32-finetuned-vqa",
+    )
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    model_path = args.model_name_or_path
+
+    if is_torch_npu_available():
+        device = "npu:0"
+    else:
+        device = "cpu"
+        
+    # Load model from HuggingFace Hub
+    
+    start_time = time.time()
+    
+    vqa_pipeline = pipeline("visual-question-answering", model=model_path, device=device)
+    url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+    image = Image.open(requests.get(url, stream=True).raw)
+    question = "How many cats are there?"
+
+    output = vqa_pipeline(image=image, question=question, top_k=2)
+    print(output)
+    
+    end_time = time.time()
+    print(f"硬件环境：{device},推理执行时间：{end_time - start_time}秒")
+    
+if __name__ == "__main__":
+    main()
