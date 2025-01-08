385242398f5b4e0cd43aea737ca717124d149174
YiChao Ding <jeffding890430@163.com>
2025-01-08 15:41:10 +0800

    change tensor-parallel-size to 8

--- MindSpeed/internlm2_5/ckpt_convert_internlm25_chat_7b_mcore2hf.sh
+++ MindSpeed/internlm2_5/ckpt_convert_internlm25_chat_7b_mcore2hf.sh
@@ -10,7 +10,7 @@ python convert_ckpt.py \
     --save-model-type hf \
     --w-pack True \
     --params-dtype bf16 \
-    --target-tensor-parallel-size 1 \
+    --target-tensor-parallel-size 8 \
     --target-pipeline-parallel-size 1 \
     --load-dir ./model_weights/internlm2_5-7b-chat-mcore-tp1-pp1/ \
     --save-dir ./model_from_hf/internlm2_5-7b-chat/ # 需要填入原始HF模型路径，新权重会存于./model_from_hf/internlm25_7b_hf/mg2hg/
